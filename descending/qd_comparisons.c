ed2a21d418564f1082205be70e545ace64bf1520
GutsJSN <jaysonbhat@gmail.com>
2025-04-01 10:11:06 +0530

    final changes

--- descending/qd_comparisons.c
+++ descending/qd_comparisons.c
@@ -24,15 +24,20 @@ void resetCounters() {
 }
 
 void bubbleSort(int arr[], int n) {
+    int swapped;
     for (int i = 0; i < n-1; i++) {
+        swapped = 0;
         for (int j = 0; j < n-i-1; j++) {
             bubble_comparisons++; // Count comparison
             if (arr[j] > arr[j+1]) {
                 int temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                swapped = 1;
             }
         }
+        if(swapped == 0)
+        break;
     }
 }
 
